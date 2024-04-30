0ab4e78af31978bda2c633cf6787633b1cd034a2
Leo Lux <leonard.lux@rwth-aachen.de>
2024-04-30 11:48:16 +0200

    found error in gamma for 3.7 a

--- problem_3/part_2/config.py
+++ problem_3/part_2/config.py
@@ -1,14 +1,17 @@
 import numpy as np
-# given in exam
-tau     = 2 * 10**-3
-lambda_value = 0.18 * 10**-3
 
-x_max = 0.01
+milli = 1
+#milli = 10**-3
+# given in exam
+tau     = 2  * milli
+lambda_value = 0.18 * milli
+
+x_max = 10  * milli
 x_min = 0
 L = x_max-x_min
 x_0 = (x_max-x_min)/2
 
-n_x = 301
+n_x = 301 
 n_t = 1000
 
 delta_t = 1e-5
@@ -17,12 +20,12 @@ x_g, delta_x = np.linspace(x_min,x_max,n_x,retstep=True,)
 t_g = np.arange(0,n_t) * delta_t
 
 # potential values
-gamma = 0.5 * 10**-3
-v_star = -40 *10**-3
-v_na_nerst = 56 * 10**-3
-v_k_nerst = -76 * 10**-3
-v_mem = -70 * 10**-3
-v_appl = -50 *10**-3
+gamma = 0.5  * milli**-1
+v_star = -40  * milli
+v_na_nerst = 56  * milli
+v_k_nerst = -76 * milli
+v_mem = -70 * milli
+v_appl = -50  * milli
 g_k = 5
 
 # used to define matricies
--- problem_3/part_2/opt_initial_values.py
+++ problem_3/part_2/opt_initial_values.py
@@ -3,9 +3,7 @@ import config as c
 
 # generate inital values
 
-def inital_values(v_appl = c.v_appl):
+def inital_values(v_appl):
     u_0 = (v_appl - c.v_mem) * np.exp(-1*(c.x_g-c.x_0)**2/(2*c.lambda_value**2)) + c.v_mem
     # scale to have correct inital mass in the system, even if part of the distribution is outside
     return u_0
-
-print(inital_values())
